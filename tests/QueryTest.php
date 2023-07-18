68a8887876e74b1570dcc2e3ac95fe6951184092
arutyunyan <arutyunyan@bolid.ru>
2023-07-18 11:26:11 +0300

    add php units

---
+++ tests/QueryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class QueryTest extends TestCase
+{
+    public function testQuery(): void
+    {
+        $this->assertTrue(true);
+    }
+}
